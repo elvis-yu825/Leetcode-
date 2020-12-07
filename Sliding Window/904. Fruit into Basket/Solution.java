631fdaa145b0c2baaaf2780a78922198dcb3236c
Elvis <61921385+elvis-yu825@users.noreply.github.com>
2020-12-06 18:51:13 -0800

    Update Solution.java

--- Sliding Window/904. Fruit into Basket/Solution.java
+++ Sliding Window/904. Fruit into Basket/Solution.java
@@ -6,8 +6,8 @@ class Solution {
         
         for (int end=0; end<tree.length; end++) {
             int right = tree[end];
-            map.put(right,map.getOrDefault(right,0) +1);
-            
+            map.put(right,map.getOrDefault(right,0) +1); // add element into the map
+            // shrink the window until we only have two type of fruits in the basket
             while (map.size() > 2) {
                 int left = tree[start];
                 map.put(left, map.get(left) -1);
@@ -15,7 +15,7 @@ class Solution {
                     map.remove(left);
                 
                 }
-                start++;
+                start++; //shrink the window
             }
             max = Math.max(max, end-start +1);
         }
