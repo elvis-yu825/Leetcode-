0c23941a519791d0f62fecb10fa81f2226d2b6c5
Elvis <61921385+elvis-yu825@users.noreply.github.com>
2020-12-06 19:03:50 -0800

    Update Solution.java

--- Sliding Window/3. Longest Substring Without Repeating Characters/Solution.java
+++ Sliding Window/3. Longest Substring Without Repeating Characters/Solution.java
@@ -8,6 +8,9 @@ class Solution {
             char right = s.charAt(end);
             if (map.containsKey(right)) {
                 start = Math.max(start,map.get(right) +1);
+                // avoid the situation like "abba":
+                //if end = 3, bc map still containes "a", start will go back to 1 pointing to first "b". 
+                //we should restrict it at least the new start should be greater than or equal to the old start
             }
             map.put(right,end);
             max = Math.max(max, end-start+1);
