5c1d8404cca9ebfb95d5363daa425f9be23c5d46
Elvis <61921385+elvis-yu825@users.noreply.github.com>
2020-12-19 12:22:03 -0800

    Create Solution.java

---
+++ Cyclic Sort/287. Find the Duplicate Number/Solution.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int findDuplicate(int[] nums) {
+        int i = 0;
+        while (i<nums.length) {
+            if (nums[i] != i+1) {
+                if (nums[i] != nums[nums[i]-1]) {
+                    swap(nums,i, nums[i]-1);
+                } else {
+                    return nums[i];
+                }
+            } else {
+                i++;
+            }
+        }
+        return -1;
+        
+    }
+    
+    private static void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
