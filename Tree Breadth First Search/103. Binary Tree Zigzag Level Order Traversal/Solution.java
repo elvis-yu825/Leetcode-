29ca6a73e693bc712b41015a709fb3c1c893ac56
Elvis <61921385+elvis-yu825@users.noreply.github.com>
2020-12-20 15:20:14 -0800

    Create Solution.java

---
+++ Tree Breadth First Search/103. Binary Tree Zigzag Level Order Traversal/Solution.java
@@ -0,0 +1,49 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+        if (root == null) return result;
+        
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        boolean leftToRight = true;
+        while (!queue.isEmpty()) {
+            int levelSize = queue.size();
+            List<Integer> currentLevel = new LinkedList<>();
+            for (int i=0; i<levelSize; i++) {
+                TreeNode currentNode = queue.poll();
+                //add the node to the currentLevel list by the current traverse direction
+                if (leftToRight) {
+                    currentLevel.add(currentNode.val);
+                } else {
+                    currentLevel.add(0, currentNode.val);
+                }
+                if (currentNode.left != null) {
+                    queue.offer(currentNode.left);
+                }
+                if (currentNode.right != null) {
+                    queue.offer(currentNode.right);
+                }
+            }
+            result.add(currentLevel);
+            //reverse the traversal direction
+            leftToRight = !leftToRight;
+            
+        }     
+        return result;
+    }
+}
