93db358f4141c4570621b1e82b93af76d3fe598d
Elvis <61921385+elvis-yu825@users.noreply.github.com>
2020-12-20 20:39:54 -0800

    Create Solution.java

---
+++ Tree Breadth First Search/515. Find Largest Value in Each Tree Row/Solution.java
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> largestValues(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        if (root == null) return result;
+        
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()){
+            int currentLevel = queue.size();
+            int currentMax = Integer.MIN_VALUE;
+            for (int i=0; i<currentLevel; i++) {
+                TreeNode currentNode = queue.poll();
+                currentMax = Math.max(currentMax, currentNode.val);
+                if (currentNode.left != null) {
+                    queue.offer(currentNode.left);
+                }
+                if (currentNode.right != null) {
+                    queue.offer(currentNode.right);
+                }
+                
+            }
+            result.add(currentMax);
+        }
+        return result;
+        
+    }
+}
