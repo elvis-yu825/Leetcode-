1b26a06b64dec0ebc6a1133d18d8cdb2522d9520
Elvis <61921385+elvis-yu825@users.noreply.github.com>
2020-12-20 21:14:55 -0800

    Create Solution.java

---
+++ Tree Breadth First Search/111. Minimum Depth of Binary Tree/Solution.java
@@ -0,0 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int minDepth(TreeNode root) {
+        if (root == null) return 0;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        int minLevel = 0;
+        while (!queue.isEmpty()) {
+            minLevel++;
+            int levelSize = queue.size();
+            for (int i=0; i<levelSize; i++) {
+                TreeNode currentNode = queue.poll();
+                if (currentNode.left == null && currentNode.right == null) {
+                    return minLevel;
+                }
+                if (currentNode.left != null) {
+                    queue.offer(currentNode.left);
+                }
+                if (currentNode.right != null) {
+                    queue.offer(currentNode.right);
+                }
+            }
+        }
+        return minLevel;
+        
+    }
+}
