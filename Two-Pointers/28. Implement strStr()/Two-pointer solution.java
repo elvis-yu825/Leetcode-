4c1944c6b6645d82afa5e8d33c118f51fdbc62e0
Elvis <61921385+elvis-yu825@users.noreply.github.com>
2021-01-02 12:02:10 -0800

    Create Two-pointer solution.java

---
+++ Two-Pointers/28. Implement strStr()/Two-pointer solution.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int strStr(String haystack, String needle) {
+        int L= needle.length(), n = haystack.length();
+        if (L == 0) return 0;
+        int pn=0;
+        while (pn <n-L+1) {
+            //find the position of the first needle character
+            //in the haystack string
+            while (pn<n-L+1 && haystack.charAt(pn) != needle.charAt(0)) {
+                pn++;
+            }
+            //compute the max match string
+            int currLen = 0, pL = 0;
+            while (pL < L && pn<n &&haystack.charAt(pn) == needle.charAt(pL)) {
+                pn++;
+                pL++;
+                currLen++;
+            }
+            if (currLen == L) return pn-L;
+            
+            pn = pn-currLen+1;
+        }
+        return -1;
+        
+    }
+}
