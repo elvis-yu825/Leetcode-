a0ef019ddcca8eb3721d1d1a077035c49776ffd8
Elvis <61921385+elvis-yu825@users.noreply.github.com>
2021-01-04 10:57:24 -0800

    Create Solution.java

---
+++ Array/48. Rotate Image/Solution.java
@@ -0,0 +1,18 @@
+class Solution {
+    public void rotate(int[][] matrix) {
+        int size = matrix.length-1;
+        for (int layer = 0; layer<(matrix.length/2) ; layer++) {
+            for (int i=layer; i < size-layer; i++){
+                int top = matrix[layer][i];
+                int right = matrix[i][size-layer];
+                int bot = matrix[size-layer][size-i];
+                int left = matrix[size-i][layer];
+                matrix[layer][i] = left;
+                matrix[i][size-layer] = top;
+                matrix[size-layer][size-i] = right;
+                matrix[size-i][layer] = bot;
+            }
+        }
+        
+    }
+}
