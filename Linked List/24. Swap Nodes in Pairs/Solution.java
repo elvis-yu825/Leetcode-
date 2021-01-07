a4a0e21cb078f73561f3cc4b0e66ec2ccfdb5eb2
Elvis <61921385+elvis-yu825@users.noreply.github.com>
2021-01-07 14:27:34 -0800

    Create Solution.java

---
+++ Linked List/24. Swap Nodes in Pairs/Solution.java
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode swapPairs(ListNode head) {
+        ListNode dumm = new ListNode();
+        dumm.next = head;
+        ListNode prev = dumm;
+        while ((head != null) && (head.next != null)) {
+            ListNode first = head;
+            ListNode second = head.next;
+            prev.next = second;
+            first.next = second.next;
+            second.next = first;
+            prev = first;
+            head = first.next;
+        }
+        return dumm.next;
+        
+    }
+    
+}
