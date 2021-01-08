c7ea84907d9fa66eb0f1162fd20c204d9d8d954d
Elvis <61921385+elvis-yu825@users.noreply.github.com>
2021-01-08 13:54:32 -0800

    Create Solution.java

---
+++ Tree/1506. Find Root of N-Ary Tree/Solution.java
@@ -0,0 +1,24 @@
+class Solution {
+    public Node findRoot(List<Node> tree) {
+
+        Integer valueSum = 0;
+
+        for (Node node : tree) {
+            // the value is added as a parent node
+            valueSum += node.val;
+            for (Node child : node.children)
+                // the value is deducted as a child node.
+                valueSum -= child.val;
+        }
+
+        Node root = null;
+        // the value of the root node is `valueSum`
+        for (Node node : tree) {
+            if (node.val == valueSum) {
+                root = node;
+                break;
+            }
+        }
+        return root;
+    }
+}
