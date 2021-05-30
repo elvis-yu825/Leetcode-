ce3d3517037c67bd9b3abe65ae095382886e645a
Elvis <61921385+elvis-yu825@users.noreply.github.com>
2021-05-30 16:27:36 -0700

    Update Solution.java

--- Sliding Window/424. Longest Repeating Character Replacement/Solution.java
+++ Sliding Window/424. Longest Repeating Character Replacement/Solution.java
@@ -1,22 +1,23 @@
 class Solution {
     public int characterReplacement(String s, int k) {
-        int start = 0;
+        int[] freq = new int[26];
+        int mostFreqLetter = 0;
+        int left = 0;
         int max = 0;
-        int result = 0;
-        Map<Character,Integer> map = new HashMap<>();
         
-        for (int end=0; end<s.length(); end++) {
-            char right = s.charAt(end);
-            map.put(right,map.getOrDefault(right,0)+1);
-            max = Math.max(max,map.get(right));
+        for(int right = 0; right < s.length(); right++){
+            freq[s.charAt(right) - 'A']++;
+            mostFreqLetter = Math.max(mostFreqLetter, freq[s.charAt(right) - 'A']);
             
-            if (end-start+1-max > k) {
-                char left = s.charAt(start);
-                map.put(left,map.get(left)-1);
-                start++;
+            int lettersToChange = (right - left + 1) - mostFreqLetter;
+            if(lettersToChange > k){
+                freq[s.charAt(left) - 'A']--;
+                left++;
             }
-            result = Math.max(result,end-start+1);
+            
+            max = Math.max(max, right - left + 1);
         }
-        return result;
+        
+        return max;
     }
 }
